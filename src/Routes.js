aa960137c9e2bd7ee1873a74ffd2af3638cd4470
LeonardoJaques <jaques.projetos@outlook.com>
2021-03-15 14:57:37 -0300

    Dog finder finish

---
+++ src/Routes.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+import DogDetails from './DogDetails';
+import DogList from './DogList';
+
+class Routes extends Component {
+  render() {
+    const getDog = (props) => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        (dog) => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />;
+    };
+
+    return (
+      <div>
+        <Switch>
+          <Route
+            exact
+            path="/dogs"
+            render={() => <DogList dogs={this.props.dogs} />}
+          />
+          <Route exact path="/dogs/:name" render={getDog} />
+          <Redirect to="/dogs" />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Routes;
